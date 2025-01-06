ccfe6d7eba79926cbe5537365cf428b04dddefea
Deepak karki <ideepakkarki@gmail.com>
2025-01-06 11:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,30 +2,32 @@ let productList = document.getElementById("product-list");
 let cartList = document.getElementById("cart-list");
 let clearCartBtn = document.getElementById("clear-cart-btn");
 
-const products = [
-  { id: 1, name: "Apple", price: 10 },
-  { id: 2, name: "Mango", price: 20 },
-  { id: 3, name: "Banana", price: 30 },
-  { id: 4, name: "Grapes", price: 40 },
-  { id: 5, name: "Cherry", price: 50 },
-];
+// const products = [
+//   { id: 1, name: "Apple", price: 10 },
+//   { id: 2, name: "Mango", price: 20 },
+//   { id: 3, name: "Banana", price: 30 },
+//   { id: 4, name: "Grapes", price: 40 },
+//   { id: 5, name: "Cherry", price: 50 },
+// ];
+
+let products = ["hello", "hello2"];
 
 // Render the list of products
-// function renderProduct() {
-//   if (!productList) return; // Prevent errors if the product list is missing
-//   productList.innerHTML = ""; // Clear the product list before rendering
-//   products.forEach((item) => {
-//     let prdListli = document.createElement("li");
-//     let addToCartBtn = document.createElement("button");
-//     addToCartBtn.innerHTML = "Add to Cart";
-//     addToCartBtn.id = `id-${item.id}`;
-//     addToCartBtn.onclick = () => addToCart(item);
+function renderProduct() {
+  if (!productList) return; // Prevent errors if the product list is missing
+  productList.innerHTML = ""; // Clear the product list before rendering
+  products.forEach((item) => {
+    let prdListli = document.createElement("li");
+    let addToCartBtn = document.createElement("button");
+    addToCartBtn.innerHTML = "Add to Cart";
+    addToCartBtn.id = `id-${item.id}`;
+    addToCartBtn.onclick = () => addToCart(item);
 
-//     prdListli.innerHTML = `${item.name} - ${item.price}`;
-//     prdListli.append(addToCartBtn);
-//     productList.append(prdListli);
-//   });
-// }
+    prdListli.innerHTML = `${item.name} - ${item.price}`;
+    prdListli.append(addToCartBtn);
+    productList.append(prdListli);
+  });
+}
 
 // Add item to the cart
 function addToCart(item) {
